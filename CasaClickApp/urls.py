eccdcbee6d2fe6417f508cce929c35fcfe6af0e1
rivascristian28@gmail.com <rivascristian28@gmail.com>
2023-08-30 17:52:08 -0500

    Base Html,Cristian

--- CasaClickApp/urls.py
+++ CasaClickApp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.landing,name="landing"),
+    path('map/', views.map_view, name='map-view'),
 ]
--- CasaClickApp/views.py
+++ CasaClickApp/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 def landing(request):
     return render(request,'base.html')
 
+def map_view(request):
+    return render(request, 'map.html')
+
 
